858ae9cb8a7d81e072392ee16f5eb4a104475ed1
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-08-31 12:57:21 +0200

    curses page created

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -6,8 +6,11 @@ const Header = () => {
   return (
     <nav>
       <NavLink to="/" activeStyle={activeStyle} exact>
-        {" "}
         Home
+      </NavLink>
+      <NavLink to="/courses" activeStyle={activeStyle} exact>
+        {" | "}
+        Courses
       </NavLink>
       <NavLink to="/about" activeStyle={activeStyle} exact>
         {" | "}

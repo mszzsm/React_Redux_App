32116c2f00874b11a0c81f78d736472c9098855c
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-09-01 12:14:48 +0200

    Redux cofigured and working properly

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,9 +1,53 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
 
 class CoursesPage extends React.Component {
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course: course });
+  };
+
+  handleSubmit = () => {
+    event.preventDefault();
+    this.props.dispatch(courseActions.createCourse(this.state.course));
+  };
+
   render() {
-    return <h2> Courses </h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2> Courses </h2>
+        <h3> Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange.bind(this)}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+        {this.props.courses.map(course => (
+          <div key={course.title}> {course.title} </div>
+        ))}
+      </form>
+    );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  dispatch: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
